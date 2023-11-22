9a94482b6948300d2f0183892861af77242b27b3
avery011 <mha0004@uah.edu>
2023-11-22 16:03:05 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,6 +21,9 @@ const App = () => {
                     <div className='col-sm'>
                         <ExpenseTotal />
                     </div>
+                            <div className='col-sm'>
+                        <CurrencyDropdown />
+                    </div>
                 </div>
                 <h3 className='mt-3'>Allocation</h3>
                 <div className='row '>
