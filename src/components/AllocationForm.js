f235448b5060a06e7851bb9fc736486aca7b3005
avery011 <mha0004@uah.edu>
2023-11-27 11:27:58 -0600

    Update AllocationForm.js

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const AllocationForm = (props) => {
     const { dispatch,remaining  } = useContext(AppContext);
     const [name, setName] = useState('');
@@ -7,7 +8,7 @@ const AllocationForm = (props) => {
     const [action, setAction] = useState('');
     const submitEvent = () => {
             if(cost > remaining) {
-                alert("The value cannot exceed remaining funds  £"+remaining);
+                alert("The value cannot exceed remaining funds  "+remaining);
                 setCost("");
                 return;
             }
