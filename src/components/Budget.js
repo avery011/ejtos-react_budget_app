d5e4e58604a256b2fd34eb5eefe62bdc7c10e8f6
avery011 <mha0004@uah.edu>
2023-11-27 11:32:35 -0600

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,26 @@
 import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+import CurrencyDropdown from './CurrencyDropdown'; // Import the CurrencyDropdown component
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
-    }
-    return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
-    );
+  const { budget, currency } = useContext(AppContext);
+  const [newBudget, setNewBudget] = useState(budget);
+
+  const handleBudgetChange = (event) => {
+    setNewBudget(event.target.value);
+  };
+
+  return (
+    <div>
+      <CurrencyDropdown /> {/* Include the CurrencyDropdown component */}
+
+      <div className='alert alert-secondary'>
+        <span>Budget: {currency}{budget}</span>
+        <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+        <span>Selected Currency: {currency}</span>
+      </div>
+    </div>
+  );
 };
+
 export default Budget;
